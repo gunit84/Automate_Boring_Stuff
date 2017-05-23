50dd567fb51cf2e22f19877975390ffee4897c94
gjones1 <gavinj84@gmail.com>
2017-05-24 09:17:17 +1000

    hello world example

---
+++ secretpassword.py
@@ -0,0 +1,16 @@
+#!python3
+# Test Python Example
+
+passwordFile = open('SecretPasswordFile.txt')
+secretPassword = passwordFile.read()
+
+print("Enter your password.")
+typedPassword = input()
+
+if typedPassword == secretPassword:
+    print("Access Granted")
+    if typedPassword == "12345"
+        print("That password is one that someone puts on their luggage")
+else:
+    print("Access Denied")
+
