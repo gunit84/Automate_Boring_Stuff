196bdf02ef6529be0e9327c23935a459ca2b6375
gjones1 <gavinj84@gmail.com>
2017-06-06 11:50:15 +1000

    hello world example

---
+++ helloFunc.py
@@ -0,0 +1,11 @@
+#!python3
+# function example
+
+def hello():
+    print("Howdy!")
+    print("Howdy!")
+    print("Hello there")
+
+hello()
+hello()
+hello()
---
+++ helloFunc2.py
@@ -0,0 +1,9 @@
+#!python3
+# Hello Function 2
+
+def hello(name):
+    print("Hello " + name)
+
+hello("Alice")
+hello("Bob")
+
